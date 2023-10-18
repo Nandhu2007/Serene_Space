4768c4d23fa973fbfa2e03534205997f598f5052
Gaurinandhan Umeshnath <gnandhan07@gmail.com>
2023-10-17 21:47:13 -0700

    changes main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:serene_space_final/pages/index.dart';
 
 import 'pages/colors.dart' as color;
 import 'pages/login.dart';
@@ -15,7 +16,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: LoginPage(),
+      home: IndexPage(),
     );
   }
 }
