eb7e2186b9ea1c7e4f252909787cbf71e567c78d
Gaurinandhan Umeshnath <gnandhan07@gmail.com>
2023-10-23 16:37:50 -0700

    adding stateless widget

--- lib/pages/chatscreen.dart
+++ lib/pages/chatscreen.dart
@@ -8,16 +8,44 @@ class ChatScreen extends StatefulWidget {
   State<ChatScreen> createState() => _ChatScreenState();
 }
 
+class Chat {
+  String text;
+  bool amsender;
+
+  Chat(this.text, this.amsender);
+}
+
 class _ChatScreenState extends State<ChatScreen> {
   late OpenAI openAI;
+  final TextEditingController _controller = TextEditingController();
+
+  final List<Chat> _conversations = [];
 
   @override
   void initState() {
-    openAI = OpenAI.instance.build(token: "key");
+    openAI = OpenAI.instance
+        .build(token: "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");
     super.initState();
   }
 
-  void send() {}
+  void send() async {
+    var text = _controller.text;
+    if (text.trim().isEmpty) {
+      return;
+    }
+
+    setState(() {
+      _conversations.add(Chat(text, true));
+    });
+    var resp = await openAI.onCompletion(
+        request: CompleteText(prompt: text, model: TextBabbage001Model()));
+
+    if (resp!.choices.isNotEmpty) {
+      setState(() {
+        _conversations.add(Chat(resp.choices.last.text, false));
+      });
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -25,19 +53,26 @@ class _ChatScreenState extends State<ChatScreen> {
       appBar: AppBar(),
       body: Column(children: [
         Expanded(
-            child: ListView(
-          children: [],
+            child: ListView.builder(
+          itemBuilder: (context, index) {
+            var convo = _conversations[index];
+            return Chip(label: Text(convo.text));
+          },
+          itemCount: _conversations.length,
         )),
         SafeArea(
-          child: Row(
-            children: [
-              Expanded(
-                  child: TextField(
-                decoration: InputDecoration(hintText: "Type Here..."),
+          child: Padding(
+              padding: EdgeInsets.all(8),
+              child: Row(
+                children: [
+                  Expanded(
+                      child: TextField(
+                    controller: _controller,
+                    decoration: InputDecoration(hintText: "Type Here..."),
+                  )),
+                  IconButton(onPressed: send, icon: Icon(Icons.send))
+                ],
               )),
-              IconButton(onPressed: send, icon: Icon(Icons.send))
-            ],
-          ),
         )
       ]),
     );
